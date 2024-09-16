cfc4fb9cb7eb1bab8b5406ad34f4d8023dd304f6
ali mehdi <ykhizar2@gmail.com>
2024-09-17 01:12:17 +0500

    add some more changes

--- src/script.js
+++ src/script.js
@@ -3,3 +3,15 @@ const navBtn = document.querySelector("#navBtn");
 navBtn.addEventListener("click", () => {
   mobileNav.classList.toggle("active");
 });
+
+
+gsap.to("#myPhotoSection", {
+  scale: 1.2,                  // Scale the section to 1.5x
+  borderRadius: "0%",           // Animate the border-radius to 0%
+  scrollTrigger: {
+    trigger: "#myPhotoSection", // Trigger when #myPhotoSection enters the viewport
+    start: "top top",           // Start when the top of the section reaches the top of the viewport
+    end: "bottom center",       // End the animation when the bottom reaches the center of the viewport
+    scrub: true,                // Smooth animation as the user scrolls
+  }
+});
