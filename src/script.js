8d7791e47e508f543f23317a8ee7ae40ee2694fa
ali mehdi <ykhizar2@gmail.com>
2024-10-03 11:56:43 +0500

    change in circle photo

--- src/script.js
+++ src/script.js
@@ -1,19 +1,7 @@
 const mobileNav = document.querySelector(".mobile-nav");
 const navBtn = document.querySelector("#navBtn");
 
-let isActive = false;
 navBtn.addEventListener("click", () => {
   mobileNav.classList.toggle("active")
 });
 
-
-gsap.to("#myPhotoSection", {
-  scale: 1.2, 
-  borderRadius: "0%", 
-  scrollTrigger: {
-    trigger: "#myPhotoSection",
-    start: "top top", 
-    end: "bottom 50%",
-    scrub: 2,
-  },
-});
