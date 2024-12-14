4a83527c584ddd03a6b3864150525e7216187464
Khizar abbas <ykhizar2@gmail.com>
2024-12-14 22:38:56 +0500

    some ui fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // import defaultTheme from "tailwindcss/defaultTheme";
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ["*"],
   theme: {
     extend: {
       colors: {
